1377c00916b32b9fed2b1ab54b41c362861dbbdd
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-05-03 11:14:19 +0200

    prevent-deletion-trigger

---
+++ prevent-deletion-trigger.sql
@@ -0,0 +1,10 @@
+DELIMITER $$
+CREATE TRIGGER prevent_deletion
+BEFORE DELETE ON users
+FOR EACH ROW
+BEGIN
+SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'deletion prevented on users table';
+END$$
+DELIMITER ;
+
+
