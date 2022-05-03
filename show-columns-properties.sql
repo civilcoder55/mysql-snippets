ef266f2f74b5980981b396f83ed881a03c964584
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-05-03 11:14:04 +0200

    show-columns-properties

---
+++ show-columns-properties.sql
@@ -0,0 +1,6 @@
+SHOW FIELDS FROM db_name.table_name where Field = 'type column name here' ;
+
+EXPLAIN db_name.table_name;
+DESCRIBE db_name.table_name;
+SHOW FIELDS FROM db_name.table_name;
+SHOW COLUMNS FROM db_name.table_name;
